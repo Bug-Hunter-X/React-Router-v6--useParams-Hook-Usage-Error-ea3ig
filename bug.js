2cacb804bdce5ff664c930ae30851a621d8839c8
BugHunter-X <askzenai@gmail.com>
2025-01-29 23:22:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,25 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* ...other routes */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function About() {
+  // Problem:  Incorrect usage of useParams hook outside Route component
+  const { name } = useParams();
+  return <div>About {name}</div>;
+}
+```
