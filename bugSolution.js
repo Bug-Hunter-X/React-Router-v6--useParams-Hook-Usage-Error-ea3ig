b04606a39ad75ab94caf73ed6d4d7e650b507b9c
BugHunter-X <askzenai@gmail.com>
2025-01-29 23:22:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,24 @@
+```javascript
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about/:name" element={<About />} />
+        {/* ...other routes */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function About() {
+  const { name } = useParams();
+  return <div>About {name}</div>;
+}
+```
